d4a928aa5edc3ebf423c0a24acc6e76ba149182f
rioobod <akanbilola@gmail.com>
2022-05-01 15:38:51 -0500

    updated user-routes and user-controller and some minor tweaks

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 // module 18
 const router = require('express').Router();
-const htmlRoutes = require('./html/html-routes');
+// const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api');
 
-router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
+
+// router.use('/', htmlRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
 
+const userRoutes = require('./api/user-routes');
 module.exports = router;
